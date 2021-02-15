72c1f46c1d71101f1b14b2a2b5ef4ce21aa4e4dd
Felix Biego <45796740+fbiego@users.noreply.github.com>
2021-02-15 22:32:24 +0300

    Update esp32_time.ino

--- examples/esp32_time/esp32_time.ino
+++ examples/esp32_time/esp32_time.ino
@@ -42,7 +42,9 @@ void loop() {
 //  Serial.println(rtc.getTimeDate());      //  (String) 15:24:38 Sun, Jan 17 2021
 //  Serial.println(rtc.getTimeDate(true));  //  (String) 15:24:38 Sunday, January 17 2021
 //
-//  Serial.println(rtc.getMillis());        //  (long)    1609459200
+//  Serial.println(rtc.getMicros());        //  (long)    723546
+//  Serial.println(rtc.getMillis());        //  (long)    723
+//  Serial.println(rtc.getEpoch());         //  (long)    1609459200
 //  Serial.println(rtc.getSecond());        //  (int)     38    (0-59)
 //  Serial.println(rtc.getMinute());        //  (int)     24    (0-59)
 //  Serial.println(rtc.getHour());          //  (int)     3     (0-12)
